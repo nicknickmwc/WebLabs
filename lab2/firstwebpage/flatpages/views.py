0c3d1034abea66fe7236b61a4c367331f7544515
nicknickmwc <nicknicklnn@yandex.ru>
2021-10-27 14:03:38 +0300

    second

---
+++ lab2/firstwebpage/flatpages/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django import template
+
+def home(request):
+    return HttpResponse(u'Hello World!', content_type="text/plain")
+def home1(request):
+    return HttpResponse(u'Hello World!123')    
+def home2(request):
+    return render(request, 'templates/index.html')           
+
